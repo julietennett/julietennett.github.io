a9d9629dcb5ccc1de17ef155b10f2166c2ae1ebf
Julie Tennett <tennett.j@gmail.com>
2018-03-13 23:03:49 -0400

    add js to change colors on hover, hide face

--- _scripts/main.js
+++ _scripts/main.js
@@ -2,4 +2,18 @@ import Nav from './modules/nav';
 
 $(document).ready(function() {
   new Nav();
+  workLinksColor();
 }); //this creates a jquery object on document then calling .ready() on that jquery object, which takes in a function tnats called afer the jquery obkect is loaded
+
+function workLinksColor() {
+  var $linkContainer = $(".link--container");
+  var $workLink = $(".work__link");
+
+  $linkContainer.on("mouseenter", function() {
+    $workLink.addClass("text-white");
+    $(this).children($workLink).removeClass("text-white");
+  })
+  $linkContainer.on("mouseleave", function() {
+    $workLink.removeClass("text-white");
+  })
+}
