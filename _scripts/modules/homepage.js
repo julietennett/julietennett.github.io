f11f72957f9671a80aac76b32b71788f967a723d
Julie Tennett <tennett.j@gmail.com>
2018-03-21 17:21:49 -0400

    tweaked the works section on homepage for scroll + footer

--- _scripts/modules/homepage.js
+++ _scripts/modules/homepage.js
@@ -8,6 +8,7 @@ class Homepage {
     var $workLink = $(".work__link");
     var $hamburger = $(".nav__hamburger--icon");
     var $hoverDesc = $(".hover__description");
+    var $footer__container = $(".footer__container");
 
     $linkContainer.on("mouseenter", function() {
       $workLink.addClass("text-white");
@@ -15,12 +16,14 @@ class Homepage {
       $(this).children($workLink).removeClass("text-white");
       $(this).children($hoverDesc).css('opacity', '1');
       $hamburger.css('stroke', 'white');
+      $footer__container.css('display', 'none');
     })
     $linkContainer.on("mouseleave", function() {
       $workLink.removeClass("text-white");
       $(this).children($workLink).removeClass("text-outline");
       $hoverDesc.css('opacity', '0');
       $hamburger.css('stroke', '#745e84');
+      $footer__container.css('display', 'flex');
     })
   }
 }
