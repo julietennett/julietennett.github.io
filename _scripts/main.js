d5f00e2fefbe39ea13b7db61bd6e12dc26e7050f
Julie Tennett <tennett.j@gmail.com>
2018-03-22 20:47:19 -0400

    THE TYPING IS WORKINGgit add .git add .

--- _scripts/main.js
+++ _scripts/main.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 }); //this creates a jquery object on document then calling .ready() on that jquery object, which takes in a function tnats called afer the jquery obkect is loaded
 
 var aboutFacts = [
+  'an interaction designer who loves the problem solving that goes on behind screens',
   'a lover of all things related to design process',
   'a designer who solves problems with, like, a lot of sticky notes',
   'someone who tags ppl in a lot of memes',
@@ -21,24 +22,24 @@ var aboutFacts = [
   'an iterater, retrospect-er, and improver',
   'a designer who loves to illustrate with Sketch’s pen tool',
   'a designer who recently learned that code is not, indeed, magic because she knows how to do it now',
-  'an interaction designer who loves the problem solving that goes on behind screens',
 ];
 
 function changeFact() {
   let $aboutText = $(".about__text");
   let $aboutFact = $(".about__text--fact");
-  // let $typer = typer('.about__text--fact', 20);
   let randomFact = aboutFacts[Math.floor(Math.random()*aboutFacts.length)];
-  // $typer.line(randomFact);
-  //
-  // var typed = new Typed('.text__about--fact', {
-  //   stringsElement: '.text__about--fact'
-  // });
+
+  var typed = new Typed('.about__text--fact', {
+    strings: [aboutFacts[0]]
+  });
 
   $aboutText.on("click", function () {
     $aboutFact.text(randomFact);
-    // $typer.back(10);
     randomFact = aboutFacts[Math.floor(Math.random()*aboutFacts.length)];
-    // $typer.continue(randomFact);
+    $(".about__text--fact").text(''); //ugh remove the current text :\
+
+    typed = new Typed('.about__text--fact', {
+      strings: [randomFact]
+    });
   });
 }
