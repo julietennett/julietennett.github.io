a60d3a881df32c5ed0bb142d89dc909abe57cc29
Julie Tennett <tennett.j@gmail.com>
2018-03-24 16:38:11 -0400

    fixed div containers on home work section

--- _scripts/modules/scroll-magic.js
+++ _scripts/modules/scroll-magic.js
@@ -1,13 +1,17 @@
+import ScrollMagic from 'scrollmagic';
+
 class ScrollAnimation {
   constructor() {
     this.$body = $('body');
+    this.controller = new ScrollMagic.Controller(); //property of this class, rather than global var
+
     if (this.$body.hasClass('home')) {
       this.homeScroll();
     }
   }
 
   homeScroll() {
-
+    new ScrollMagic.Scene()
   }
 
 
