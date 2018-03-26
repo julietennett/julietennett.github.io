f243f6849d48255b055a50efcf135f29318fb526
Julie Tennett <tennett.j@gmail.com>
2018-03-26 13:15:13 -0400

    so the function WORKS technically but the math isn't right

--- _scripts/modules/img-hover.js
+++ _scripts/modules/img-hover.js
@@ -7,10 +7,19 @@ class ImageHover {
     let $image = $('.scroll-image');
     let $pageSection = $('.page-section');
 
-    $image.hover( function () {
-      $(this).children($pageSection).css('background-position', '30%, 0');
+    $image.mousemove( function(evt) {
+      let xPos = evt.pageX;//works thank god
+      // console.log(evt.pageX);
+      // console.log(xPos);
+      let imageLength = '4372';
+      // let xTest = 3;
+
+      $(this).children($pageSection).css('background-position-x',  function() {
+        return - (xPos * imageLength) + '%';
+      });
     });
   }
+
 
 }
 
