98b729cc91203a91fb23ec6277363e8ac8e48d54
Julie Tennett <tennett.j@gmail.com>
2018-03-26 20:33:46 -0400

    i think this is as good as trailtag's is gonna get

--- _scripts/modules/img-hover.js
+++ _scripts/modules/img-hover.js
@@ -1,9 +1,10 @@
 class ImageHover {
   constructor() {
-    this.scrollImage();
+    this.scrollImageX();
+    this.scrollImageXY();
   }
 
-  scrollImage() {
+  scrollImageX() {
     let $image = $('.hover-image-x');
     let $pageSection = $('.page-section');
 
@@ -15,6 +16,29 @@ class ImageHover {
 
       $(this).children($pageSection).css('background-position-x',  function() {
         return - (xPerc * 100) + '%';
+      });
+    });
+  }
+
+  scrollImageXY() {
+    let $imageXY = $('.hover-image-xy');
+    let $pageSection = $('.page-section');
+
+    $imageXY.mousemove( function(evt) {
+      let xPos = evt.pageX;//works thank god
+      let yPos = evt.pageY;
+      let pageW = $(document.body).width();
+      let xPerc = xPos / pageW;
+      let yPerc = yPos / pageW;
+
+
+      $(this).children($pageSection).css('background-position-x',
+      function() {
+        return xPerc * 100 + '%';
+      });
+      $(this).children($pageSection).css('background-position-y',
+      function() {
+        return yPerc * 100 + '%';
       });
     });
   }
