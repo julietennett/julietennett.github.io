d8e9d0cb26524cd594ca3613d7a57ab7f204d59c
Julie Tennett <tennett.j@gmail.com>
2018-04-03 21:41:04 -0400

    trying to default to transition none

--- _scripts/main.js
+++ _scripts/main.js
@@ -4,19 +4,19 @@ import About from './modules/about';
 import ScrollAnimation from './modules/scroll-magic'
 import ImageHover from './modules/img-hover'
 
+// $('body').addClass('before-load');
+
 $(document).ready(function() {
   new Nav();
   new Homepage();
   new About();
   new ImageHover();
+  // $('body').removeClass('before-load');
   setTimeout(function(){
     $('.before-load').removeClass('before-load');
     $('.before-load-left').removeClass('before-load-left');
     $('.before-load-right').removeClass('before-load-right');
   }, 200);
-  // $('.before-load').removeClass('before-load');
-  // $('.before-load-left').removeClass('before-load-left');
-  // $('.before-load-right').removeClass('before-load-right');
 
   var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
   if (isSafari) $('body').addClass('safari');
